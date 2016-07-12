1112084c4c32fbba1879874e751b721b2deb7aac
willriggins <williamriggins2013@gmail.com>
2016-07-12 11:35:45 -0400

    Initial Import

---
+++ README.md
@@ -0,0 +1,7 @@
+# cards-clojure
+
+A Clojure app to ... well, that part is up to you.
+
+## Usage
+
+FIXME
---
+++ src/cards_clojure/core.clj
@@ -0,0 +1,31 @@
+(ns cards-clojure.core
+  (:gen-class))
+
+(def suits [:clubs :spades :hearts :diamonds])
+(def ranks (range 1 14))
+
+(defn create-deck []
+  (set
+    (for [suit suits
+          rank ranks]
+      {:suit suit :rank rank})))
+
+(defn create-hands [deck]
+  (set
+    (for [c1 deck
+          c2 (disj deck c1)
+          c3 (disj deck c1 c2)
+          c4 (disj deck c1 c2 c3)]
+      #{c1 c2 c3 c4})))
+
+(defn flush? [hand]
+  (let [suits (set (map :suit hand))]
+    (= 1 (count suits))))
+  
+
+
+(defn -main []
+  (let [deck (create-deck)
+        hands (create-hands deck)
+        flushes (filter flush? hands)]
+    (count flushes)))
